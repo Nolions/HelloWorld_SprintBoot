7f2f064733afc54622ecf5259037a0dd01949264
nolions <pkh_nolions@photons.com.tw>
2023-05-29 16:14:41 +0800

    [init]

---
+++ src/main/java/com/nolions/hellowordsprintboot/HellowordSprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.nolions.hellowordsprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowordSprintBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowordSprintBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nolions/hellowordsprintboot/HellowordSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nolions.hellowordsprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowordSprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
